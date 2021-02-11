5860ed779eb05ff6708cb6e4d7ca4a79d683ba41
Eric <ericcleptonsilva@gmail.com>
2021-02-10 21:08:29 -0300

    Mobx com 90% implantado

--- lib/stores/list_store.dart
+++ lib/stores/list_store.dart
@@ -24,10 +24,53 @@ abstract class _ListStore with Store {
   void setPrice(String value) => price = value;
 
   @computed
-  bool get isFormValid =>
-      photo.isEmpty &&
-      model.isEmpty &&
-      brand.isEmpty &&
-      year.isEmpty &&
-      price.isEmpty;
+  bool get photoValid => photo != null && photo.length > 2;
+  String get photoError {
+    if (photo == null || photoValid)
+      return null;
+    else if (photo.isEmpty)
+      return "Campo obrigatório!";
+    else
+      return "Nome muito Curto";
+  }
+
+  bool get modelValid => model != null && model.length > 2;
+  String get modelError {
+    if (model == null || modelValid)
+      return null;
+    else if (model.isEmpty)
+      return "Campo obrigatóri!";
+    else
+      return "Nome muito Curto";
+  }
+
+  bool get brandValid => brand != null && brand.length > 2;
+  String get brandError {
+    if (brand == null || brandValid)
+      return null;
+    else if (brand.isEmpty)
+      return "Campo obrigatório!";
+    else
+      return "Nome muito Curto";
+  }
+
+  bool get yearValid => year != null && year.length > 3;
+  String get yearError {
+    if (year == null || yearValid)
+      return null;
+    else if (year.isEmpty)
+      return "Campo obrigatório! Ex.: 2021";
+    else
+      return "Ano muito Curto";
+  }
+
+  bool get priceValid => price != null && price.length > 9;
+  String get priceError {
+    if (price == null || priceValid)
+      return null;
+    else if (price.isEmpty)
+      return "Campo obrigatório! Ex.: R\$30.000,00";
+    else
+      return "Valor muito Curto";
+  }
 }
